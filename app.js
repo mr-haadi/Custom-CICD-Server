98d8db02f6a01832917b4f206d58337d733b6690
Saif Raja <saifraja1611@gmail.com>
2026-07-05 18:52:14 +0000

    added get endpoint

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ import { githubRoutes } from "./routes/github.js";
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+	return res.json({message: "cicd server is running fine on production.."})
+});
+
 app.post("/webhooks/github", githubRoutes);
 
 
